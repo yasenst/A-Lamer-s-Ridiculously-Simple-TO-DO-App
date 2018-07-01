8774e0fe94888bec862ad7298bb4f181e5b0b67d
Yasen <yasen9977@gmail.com>
2018-07-01 15:57:36 +0300

    functionality done, only css remains

--- experimentalscript.js
+++ experimentalscript.js
@@ -1,6 +1,5 @@
 /*$(function(){*/
-    /*$('.description').hide();*/
-
+    
     $('.new').submit(function () {
         if (   $('#new').val() !== ''
             && $('#todo-description').val() !== ''
@@ -8,24 +7,14 @@
             var itemTitle = $('#new').val();
             var itemDate = $('#datetimepicker').val();
             var itemDescription = $('#todo-description').val();
-            var input = '<div class="task-value"><input type="text" value="' + itemTitle + '" readonly/><span class="tooltiptext">Click to expand!</span></div>';
+            var input = '<div class="task-value"><input type="text" value="' + itemTitle + '" readonly/></div>';
             var edit = '<ol><li class="description">'+itemDescription+'</li><li class="due-date">'+itemDate+'</li><li class="check fa fa-check"></li><li class="delete fa fa-times"></li></ol>';
             $('.list').append('<li class="item">' + input + edit + '</li>');
         };
         $('#new').val('');
         $('#todo-description').val('');
         $('#datetimepicker').val('');
-        /*
-        $('#list .list .item').each(function(e){
-            var hue = ( e * 3 );
-            $(this).css({ backgroundColor: 'hsl(' + hue + ',75%,50%)' });
-            $('ol li', this).each(function(i){
-              var o = .1 + ( i * .1 );
-              $(this).css({ backgroundColor: 'rgba(0,0,0,' + o + ')' });
-            });
-          });
-          */
-
+       
           $('#list .list .item ol li').on('click',function(){
             if ( $(this).hasClass('check') ) {
               $(this).parent('ol').parent('.item').css("background", "lime");
@@ -91,5 +80,6 @@
           $.showAlert();
           $.deleteAlert();
           });
+
         
 /*});*/
